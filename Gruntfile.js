6076bb3b532c2515b071ec45b0c7165e020c866e
Dale Corns <drcorns@hotmail.com>
2015-05-25 18:18:51 -0700

    add sass compilation and styles copy to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,13 @@
  */
 'use strict';
 module.exports = function(grunt){
+
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-add-view');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     add_view: {
@@ -30,13 +33,35 @@ module.exports = function(grunt){
         dest: 'development/bundle.js'
       }
     },
+    sass:{
+      prod:{
+        src: ['app/sass/main.sass'],
+        dest: 'production/styles/main.css'
+      },
+      dev:{
+        src: ['app/sass/main.sass'],
+        dest: 'development/styles/main.css'
+      }
+    },
     copy:{
       prod:{
-        src: ['index.html', 'app/styles/**/*.*'],
+        src: 'index.html',
         dest: 'production/'
       },
       dev:{
-        src: ['index.html', 'app/styles/**/*.*'],
+        src: 'index.html',
+        dest: 'development/'
+      },
+      prod_styles:{
+        expand: true,
+        cwd: 'app/',
+        src: 'styles/**/*.css',
+        dest: 'production/'
+      },
+      dev_styles:{
+        expand: true,
+        cwd: 'app/',
+        src: 'styles/**/*.css',
         dest: 'development/'
       },
       prod_images:{
@@ -57,10 +82,10 @@ module.exports = function(grunt){
       }
     }
   });
-  grunt.registerTask('build',['add_view', 'browserify', 'copy']);
-  grunt.registerTask('build:prod',['add_view:prod', 'browserify:prod', 'copy:prod']);
-  grunt.registerTask('build:dev',['add_view:dev', 'browserify:dev', 'copy:dev']);
-  grunt.registerTask('build:prod:all',['add_view:prod', 'browserify:prod', 'copy:prod', 'copy:prod_images', 'copy:prod_video']);
-  grunt.registerTask('build:dev:all',['add_view:dev', 'browserify:dev', 'copy:dev', 'copy:dev_images', 'copy:dev_video']);
+  grunt.registerTask('build:all',['add_view', 'sass', 'browserify', 'copy']);
+  grunt.registerTask('build:prod',['add_view:prod', 'sass:prod', 'browserify:prod', 'copy:prod']);
+  grunt.registerTask('build:dev',['add_view:dev', 'sass:dev', 'browserify:dev', 'copy:dev']);
+  grunt.registerTask('build:prod:all',['add_view:prod', 'browserify:prod', 'sass:prod', 'copy:prod', 'copy:prod_images', 'copy:prod_video']);
+  grunt.registerTask('build:dev:all',['add_view:dev', 'browserify:dev', 'sass:dev', 'copy:dev', 'copy:dev_images', 'copy:dev_video']);
 
 };
